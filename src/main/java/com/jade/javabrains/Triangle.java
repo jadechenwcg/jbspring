26dfdb3f655f9321ef361759204f653fd4e92cb1
Jade Chen <jade.chen@na.whiteclarkegroup.com>
2017-11-30 11:37:28 -0500

    first commit with reference bean

---
+++ src/main/java/com/jade/javabrains/Triangle.java
@@ -0,0 +1,31 @@
+package com.jade.javabrains;
+
+public class Triangle {
+	
+	private Point pA;
+	private Point pB;
+	private Point pC;
+	
+	public Point getpA() {
+		return pA;
+	}
+	public void setpA(Point pA) {
+		this.pA = pA;
+	}
+	public Point getpB() {
+		return pB;
+	}
+	public void setpB(Point pB) {
+		this.pB = pB;
+	}
+	public Point getpC() {
+		return pC;
+	}
+	public void setpC(Point pC) {
+		this.pC = pC;
+	}
+	
+	public void draw(){
+		System.out.println("Traingle drawn [" + getpA().getX() + "," +getpA().getY() + "]" + "[" + getpB().getX() + "," +getpB().getY() + "]" + "[" + getpC().getX() + "," +getpC().getY() + "]");
+	}
+}
